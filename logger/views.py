bee663cac8c8ddff3a360049e886a043c0285a91
michareichmann <micha.reichmann@gmail.com>
2021-08-25 13:06:05 +0200

    update views

--- logger/views.py
+++ logger/views.py
@@ -8,15 +8,25 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_run_logs'
 
     def get_queryset(self):
-        """Return the last five logs."""
-        return RunInfo.objects.order_by('-endtime')[:20]
+        """Return the last 20 run logs."""
+        return RunInfo.objects.order_by('-pk')[:20]
+
+
+def get_field_display(model, exclude=()):
+    fields = [field.name for field in model._meta.get_fields() if field.name not in ('id',) + exclude]
+    values = [getattr(model, f'get_{field}_display')() if hasattr(model, f'get_{field}_display') else getattr(model, field) for field in fields]
+    return {model._meta.get_field(field).verbose_name: value for field, value in zip(fields, values)}
 
 
 def detail(request, runnr):
     runinfo = get_object_or_404(RunInfo, runnr=runnr)
-    return render(request, 'logger/detail.html', {'runinfo': runinfo})
+    duts = [get_field_display(dut, exclude=('runinfo',)) for dut in runinfo.dut_set.all()]
+    run_display = get_field_display(runinfo, exclude=('dut', 'runnr'))
+    return render(request, 'logger/detail.html', {'runinfo': run_display, 'duts': duts, 'run': runinfo.runnr})
 
 
 def all_(request):
-    results = list(RunInfo.objects.all())
+    results = [{'run': runinfo.runnr,
+                'runinfo': get_field_display(runinfo, exclude=('dut', 'runnr')),
+                'duts': [get_field_display(dut, exclude=('runinfo',)) for dut in runinfo.dut_set.all()]} for runinfo in RunInfo.objects.all()]
     return render(request, 'logger/all.html', {'results': results})
