d1ff70e11fc1c204e4806958bba54c69a5e8221d
michareichmann <micha.reichmann@gmail.com>
2021-08-25 10:44:37 +0200

    check if files exist

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-rm logger/migrations -r
-rm db.sqlite3
+# remove migrations and the database
+if [ -f logger/migrations ]; then rm logger/migrations -r; fi
+if [ -f db.sqlite3 ]; then rm db.sqlite3 -r; fi
+
 ./manage.py makemigrations logger
-./manage.py migrate logger
 ./manage.py migrate
 ./manage.py createsuperuser
