6f276bd3e687467916fb0bafe15d13b4e583f933
michareichmann <micha.reichmann@gmail.com>
2021-08-27 15:15:40 +0200

    use whitelist gitignore and remove old files

--- reload_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm logger/migrations -r
-rm db.sqlite3
-python2 manage.py makemigrations logger
-python2 manage.py migrate logger
-python2 manage.py migrate
-python2 manage.py createsuperuser
